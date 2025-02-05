70d1c65786610052fe4f42fe296005b3c4e885a6
mffree <matthewfreeman63@gmail.com>
2025-02-05 05:45:07 +0000

    first final attempt

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ get("/") do
   erb(:square)
 end
 
+# Square
 get("/square/new") do
   erb(:square)
 end
@@ -13,21 +14,51 @@ get("/square/results") do
   @inputs = params
   @result_name = "Square"
   @result = params.fetch("number").to_f ** 2
-  erb(:results)
+  erb(:square_results)
 end
 
-
+# Square root
 get("/square_root/new") do
-
-  erb(...)
+  erb(:square_root)
 end
 
+get("/square_root/results") do
+  @inputs = params
+  @result_name = "Square Root"
+  @result = (params.fetch("number").to_f ** 0.5).round(4)
+  erb(:square_root_results)
+end
+
+# Random
 get("/random/new") do
-
-  erb(...)
+  erb(:random)
 end
 
-get("/payment/new") do
+get("/random/results") do
+  @inputs = params
+  @result_name = "Random Number"
+  min_input = params.fetch("minimum").to_f
+  max_input = params.fetch("maximum").to_f
+  @result = rand(min_input..max_input)
+  erb(:random_results)
+end
 
-  erb()
+
+# Random
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @inputs = params # apr, years, principal
+  apr_monthly = @inputs.fetch("apr").to_f / 100 / 12
+  months = @inputs.fetch("years").to_i * 12
+
+  numer = apr_monthly * @inputs[:principal].to_f
+  denom = 1 - (1 + apr_monthly) ** (months * (-1))
+  @result = numer / denom
+
+  @result_name = "Payment"
+
+  erb(:payment_results)
 end
